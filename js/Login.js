935eecb8555284e19852f356387b837e1e3344f5
tul08663 <tul08663@temple.edu>
2022-12-07 17:26:28 -0500

    Login.Js href

--- js/Login.js
+++ js/Login.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
+    ShowLogin();
+});
+
+function ShowLogin(){
     $('#LoginDiv').show();
     $('#AcodeDiv').hide();
     $('#RegisterDiv').hide();
-});
+}
+
+function ShowRegister(){
+    $('#LoginDiv').hide();
+    $('#AcodeDiv').hide();
+    $('#RegisterDiv').show();
+}
+
+function ShowAcode(){
+    $('#LoginDiv').hide();
+    $('#AcodeDiv').show();
+    $('#RegisterDiv').hide();
+}
+
+$('.GoToRegister').click(function(){
+    ShowRegister();
+})
+
+$('.GoToLogin').click(function(){
+    ShowLogin();
+})
